f3438579f10074dfba575e90115d824c8200f16f
Tomas Beranek <tomas.beranek02@gmail.com>
2023-02-25 17:12:07 +0100

    feat: added backend

---
+++ backend/src/main/java/home/project/tomaber/MeCCG/MeCcgApplication.java
@@ -0,0 +1,13 @@
+package home.project.tomaber.MeCCG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeCcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeCcgApplication.class, args);
+	}
+
+}
