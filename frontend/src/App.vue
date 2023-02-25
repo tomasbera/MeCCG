29c42900f6547a58bbb7e8333c045b4e66adfc92
Tomas Beranek <tomas.beranek02@gmail.com>
2023-02-25 18:22:01 +0100

    feat: picture fits div

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -43,10 +43,11 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
   background-image: url("./assets/rivendell.png");
   background-size: cover;
+  background-position: center;
   opacity: 0.5;
   z-index: -1;
 }
